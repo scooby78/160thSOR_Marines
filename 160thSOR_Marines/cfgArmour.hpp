2e14882f51321d25f6a066c486489f3372ca784b
scooby78 <scooby78@users.noreply.github.com>
2016-03-06 00:02:10 +1000

    Loadout changes
    Created vehicle loadout to conform with std armor loadout

--- 160thSOR_Marines/cfgArmour.hpp
+++ 160thSOR_Marines/cfgArmour.hpp
@@ -7,8 +7,7 @@
 
 	class rhsusf_m1a1fep_wd;
 
-
-//M2A3 (BUSK III)	
+//M1A1	
 	class SOR_rhsusf_m1a1fep_wd : rhsusf_m1a1fep_wd
 	{
 		faction = SOR_Faction_M;
@@ -16,21 +15,23 @@
 		class UserActions{};
 		class TransportItems
 		{
-		pack_xx(SOR_Repair_Pack_D,1) 
+			item_xx(ACE_FieldDressing,20);
 		};
 		class TransportWeapons
 		{
-			weap_xx(M3CG,2)
+			weap_xx(rhs_weap_M136_hp,2)
+			weap_xx(rhs_weap_m4a1_carryhandle_grip,2)
 		};
 		class TransportMagazines
 		{
 			mag_xx(30Rnd_556x45_Stanag_Tracer_Red,20)
-			mag_xx(MAAWS_HEAT,10)
-			mag_xx(MAAWS_HEDP,10)
-			mag_xx(rhsusf_200Rnd_556x45_soft_pouch,2)
-			mag_xx(rhsusf_100Rnd_762x51,2)
+			mag_xx(rhs_200rnd_556x45_M_SAW,4)
+			mag_xx(rhsusf_100Rnd_762x51,4)
 		};
-		class TransportBackpacks{};
+		class TransportBackpacks
+		{
+			pack_xx(SOR_Repair_Pack_D,1)			
+		};
 	};
 
 
